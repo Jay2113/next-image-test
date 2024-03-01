1ec9c730e0ead3a4fd178af2b601376314f2ae8a
Jay Raval <jay.ambrish.raval@gmail.com>
2024-02-29 19:06:59 -0500

    add remote pattern

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'main.d28zm4sv1645bb.amplifyapp.com',
+        port: '',
+        pathname: '/_next/*',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
 }
