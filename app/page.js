699afe58342c66acf0805326bce4f6179a017e76
Jay Raval <jay.ambrish.raval@gmail.com>
2024-03-06 17:00:03 -0500

    alt change

--- app/page.js
+++ app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import amplifyPic from "../public/amplify.png";
 
 export default function Home() {
-  return <Image src="https://cms-tc.pbskids.org/global/theme-backgrounds/RJGO-background-kitchen-v2@6631-×-1608.jpg" alt="PBS Image Asset" width={7680} quality={75} />;
+  return <Image src="https://cms-tc.pbskids.org/global/theme-backgrounds/RJGO-background-kitchen-v2@6631-×-1608.jpg" alt="Image Asset" width={7680} quality={75} />;
 }
