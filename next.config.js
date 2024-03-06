7b0f5d24f02cc613ffe7d22e4cfe8733a15e09ad
Jay Raval <jay.ambrish.raval@gmail.com>
2024-03-06 16:44:04 -0500

    disabling asset hash

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   images: {
     remotePatterns: [
@@ -11,4 +12,20 @@ module.exports = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes : [7680],
   },
+  webpack(config, options) {
+    if (!options.dev) {
+        // Remove content hash from JavaScript file names
+        config.output.filename = config.output.filename.replace('-[contenthash]', '');
+        config.output.chunkFilename = config.output.chunkFilename.replace('.[contenthash]', '');
+
+        // Remove hash from CSS file names
+        const NextMiniCssExtractPlugin = config.plugins.find((plugin) => plugin.constructor.name === 'MiniCssExtractPlugin');
+        if (NextMiniCssExtractPlugin) {
+            NextMiniCssExtractPlugin.options.filename = NextMiniCssExtractPlugin.options.filename.replace('[contenthash]', '[name]');
+            NextMiniCssExtractPlugin.options.chunkFilename = NextMiniCssExtractPlugin.options.chunkFilename.replace('[contenthash]', '[name]');
+        }
+
+        return config;
+    }
+  }
 }
